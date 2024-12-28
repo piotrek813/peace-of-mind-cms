ca8e93fca0c84f88d2c2da96f9872c4164104765
Piotr Smoliński <piotreksmolinski04@gmail.com>
2024-12-28 19:51:42 +0100

    fix url prefix

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 define('BASE_PATH', __DIR__);
-define('BASE_URL', isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) ? 'https://' : 'http://'. $_SERVER['HTTP_HOST']);
 define('URL_PREFIX', str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']));
+define('BASE_URL', isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) ? 'https://' : 'http://'. $_SERVER['HTTP_HOST'] . URL_PREFIX);
 
 require './vendor/autoload.php';
 require_once './src/Helpers/view.php';
